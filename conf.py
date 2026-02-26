fcedf383e5908523408a55b2cba3e1aef07ecebd
Christian Hestehave Degn <75323666+ChristianHDegn@users.noreply.github.com>
2026-02-26 15:07:30 +0100

    Update conf.py
    Added .github/instructions path to exclude paths to prepare for AI agent instructions

--- conf.py
+++ conf.py
@@ -77,7 +77,7 @@ author = 'Omnia Digital Workplace'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build','sample/*','samples/*']
+exclude_patterns = ['_build','sample/*','samples/*','.github/instructions/*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
